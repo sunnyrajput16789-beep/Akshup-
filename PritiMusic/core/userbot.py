5f522d8147fb23d380174566959870d7631ca9d3
sunnyrajput16789-beep <sunnyrajput16789@gmail.com>
2026-07-20 12:49:44 +0530

    Update chat join logic in userbot.py

--- PritiMusic/core/userbot.py
+++ PritiMusic/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("LuckyXSupport")
-                await self.one.join_chat("LuckyXSupport")
+                await self.one.join_chat("about_scam01")
+                await self.one.join_chat("about_scam01")
             except:
                 pass
             assistants.append(1)
